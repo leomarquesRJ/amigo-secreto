3b453317c5b39e5ab5854d11622271fc4e655b65
leomarquesRJ <leonardomarques.x@gmail.com>
2025-01-29 18:04:27 -0300

    adidionado botão restart

--- app.js
+++ app.js
@@ -10,19 +10,21 @@ let listAmigos = [];
 
 
 
+
 // Funcão para adicionar nome a lista de amigos "listAmigos"
 function addAmigo () {
    let nomeAdicionado = document.querySelector('input').value;
 
-   if (nomeAdicionado == ""){
+   if (nomeAdicionado == ""){ // Caso o campo input esteja vazio retorna um popup de alerta
     alert("Insira um nome válido!");
     limparInput();
    } 
-   else 
-   {
+   else {// Adiciona o nome digitado a lista de amigo e exibe o nome no navegador
     listAmigos.push(nomeAdicionado);
+    
     let listaComQuebraLinha = listAmigos.join('<br>');
-    exibirLista('listaAmigos', listaComQuebraLinha);
+    
+    exibirTexto('listaAmigos', listaComQuebraLinha);
     limparInput();
    };
 
@@ -30,14 +32,39 @@ function addAmigo () {
 };
 
 
-
+// Função para limpar campo do input
 function limparInput () {
     document.querySelector('input').value = '';
 };
 
 
-
-function exibirLista (tag, texto) {
+// Função para exibir os nomes digitados no navegador
+function exibirTexto (tag, texto) {
     document.getElementById(tag).innerHTML = texto;   
 };
+
+
+
+// Sorteia um nome da lista de amigos e caso a lista esteja vazia mostra um alerta
+function sortearAmigo() {
+    if (listAmigos == '') {
+        alert('Nenhum amigo para sortear :(');
+    }
+    else {
+    amigoSorteado =  listAmigos[Math.floor(Math.random() * listAmigos.length)];
+    exibirTexto('listaAmigos', '');
+    exibirTexto(`resultado`, `O nome sorteado é:  ${amigoSorteado}`);
+    };
+};
+
+
+
+// Função restart limpa os campos para um novo sorteio
+function restart() {
+    listAmigos = [];
+    limparInput();
+    exibirTexto('resultado', '');
+};
+
+
     
